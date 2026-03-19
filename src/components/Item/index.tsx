205797db5cf5f910e7dd8d5c8559c418eaad9ccc
xsalles <xs.salles@gmail.com>
2026-03-19 11:48:52 -0300

    refactored(component): involved my icon inside a touchable opacity

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,21 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import StatusIcon from "@/components/StatusIcon";
 import { MaterialIcons } from "@expo/vector-icons";
 import { ItemProps } from "@/types/components/Item";
 import { styles } from "./styles";
 
 export default function Item({ ...props }: ItemProps) {
-    return (
-
-  <View style={styles.container}>
-    <View style={styles.containerInfo}>
-      <StatusIcon
-        ownStatus={props.ownStatus}
-        statusSelected={props.statusSelected}
-      />
-      <Text style={styles.nameItem}>{props.name}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerInfo}>
+        <StatusIcon
+          ownStatus={props.ownStatus}
+          statusSelected={props.statusSelected}
+        />
+        <Text style={styles.nameItem}>{props.name}</Text>
+      </View>
+      <TouchableOpacity>
+        <MaterialIcons name="delete" size={24} />
+      </TouchableOpacity>
     </View>
-    <MaterialIcons name="delete" size={24} />
-  </View>
-    )
+  );
 }
