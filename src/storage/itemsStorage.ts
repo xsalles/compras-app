c95dc1b9f156c2f914c57cedbd97952a49ee41a4
xsalles <xs.salles@gmail.com>
2026-03-20 12:08:02 -0300

    feat(asyncStorage): created the function of getting items just by status

--- src/storage/itemsStorage.ts
+++ src/storage/itemsStorage.ts
@@ -4,33 +4,43 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const ITEMS_KEY = "@compras_app:items";
 
 export type ItemStorageProps = {
-    id: number;
-    status: FilterStatus;
-    name: string;
-}
+  id: number;
+  status: FilterStatus;
+  name: string;
+};
 
 export async function getAllItems(): Promise<ItemStorageProps[]> {
-    try {
-        const response = await AsyncStorage.getItem(ITEMS_KEY);
-    
-        return response ? JSON.parse(response) : [];
-    } catch (error) {
-        throw new Error("Não foi possível carregar os itens.");
-    }
+  try {
+    const response = await AsyncStorage.getItem(ITEMS_KEY);
+
+    return response ? JSON.parse(response) : [];
+  } catch (error) {
+    throw new Error("Não foi possível carregar os itens.");
+  }
 }
 
 export async function saveItem(item: ItemStorageProps): Promise<void> {
+  try {
+    const items = await getAllItems();
+
+    const newItem = {
+      id: items.length + 1,
+      name: item.name,
+      status: item.status,
+    };
+
+    await AsyncStorage.setItem(ITEMS_KEY, JSON.stringify([...items, newItem]));
+  } catch (error) {
+    throw new Error("Não foi possível salvar o item.");
+  }
+}
+
+export async function getByStatus(status: FilterStatus): Promise<ItemStorageProps[]> {
     try {
         const items = await getAllItems();
 
-        const newItem = {
-            id: items.length + 1,
-            name: item.name,
-            status: item.status,
-        }
-
-        await AsyncStorage.setItem(ITEMS_KEY, JSON.stringify([...items, newItem]));
+        return items.filter((item) => item.status === status);
     } catch (error) {
-        throw new Error("Não foi possível salvar o item.");
+        throw new Error("Não foi possível carregar os itens.");
     }
 }
